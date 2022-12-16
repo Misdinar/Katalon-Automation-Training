fb3503efa44fd91392094be306b3249b91c3ae58
Thomas Dwi <mastomcsc23@gmail.com>
2022-12-16 15:45:23 +0800

    add regist test case

---
+++ Scripts/Registration Success/Script1671127164177.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://www.psegameshop.com/')
+
+WebUI.click(findTestObject('Page_Home - PS Enterprise Gameshop/icon_user'))
+
+WebUI.selectOptionByValue(findTestObject('Page_1 new message/select_genre'), 'Action', false)
+
+WebUI.selectOptionByValue(findTestObject('Page_1 new message/select_console'), 'PC', false)
+
+WebUI.selectOptionByValue(findTestObject('Page_Home - PS Enterprise Gameshop/select_hobby'), 'Culinary', false)
+
+WebUI.check(findTestObject('Page_1 new message/input_Female'))
+
+WebUI.setText(findTestObject('Page_Home - PS Enterprise Gameshop/input__email'), 'acgsihb877@qemail.org')
+
+WebUI.setText(findTestObject('Page_1 new message/input_password_reg'), 'TestPassword123.')
+
+WebUI.setText(findTestObject('Page_1 new message/input_confirm_password'), 'TestPassword123.')
+
+WebUI.click(findTestObject('Page_1 new message/button_Register'))
+
+WebUI.verifyElementText(findTestObject('Page_1 new message/pesan_register'), 'Thank you for registering. Your account has to be activated before you can login. Please check your email.')
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Registration Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Registration Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>13031b53-2f37-4ae5-81e3-18beb2c2457e</testSuiteGuid>
+</TestSuiteEntity>
